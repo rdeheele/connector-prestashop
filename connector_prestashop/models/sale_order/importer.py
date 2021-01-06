9346d4ced97e07c07aa4273c7cd0b40da9cf744d
Romain Deheele <romain@avosdim.com>
2021-01-06 22:15:38 +0100

    [UPD] comment discounts

--- connector_prestashop/models/sale_order/importer.py
+++ connector_prestashop/models/sale_order/importer.py
@@ -173,9 +173,9 @@ class SaleOrderImportMapper(Component):
     children = [
         (_get_sale_order_lines,
          'prestashop_order_line_ids', 'prestashop.sale.order.line'),
-        (_get_discounts_lines,
-         'prestashop_discount_line_ids', 'prestashop.sale.order.line.discount')
     ]
+    """    (_get_discounts_lines,
+         'prestashop_discount_line_ids', 'prestashop.sale.order.line.discount')"""
 
     def _map_child(self, map_record, from_attr, to_attr, model_name):
         source = map_record.source
